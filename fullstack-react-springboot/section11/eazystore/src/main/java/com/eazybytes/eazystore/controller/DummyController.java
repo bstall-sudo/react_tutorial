8f6f1c8742ac86451fd21d87f9ce7735ec2485fa
@bertold.stallmach <bertold.stallmach@uzh.ch>
2026-02-11 17:09:12 +0100

    HTTP requests

--- fullstack-react-springboot/section11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
+++ fullstack-react-springboot/section11/eazystore/src/main/java/com/eazybytes/eazystore/controller/DummyController.java
@@ -15,6 +15,12 @@ public class DummyController {
         return "User created successfully";
     }
 
+    @GetMapping("/headers")
+    public String readHeaders(@RequestHeader(name="User-Agent") String userAgent, @RequestHeader(name="User-Location", required = false) String userLocation){
+
+        return "Received headers with value: " + userAgent + " at: " + userLocation;
+    }
+
     /*without @RequestParam(required = false) the request http://localhost:8080/api/v1/dummy/search (without search value
     will lead to an error. to accept requests without params you need to: @RequestParam(required = false)
     defaultValue = "Guest"
@@ -33,5 +39,22 @@ public class DummyController {
     @GetMapping("/multiple-search-map")
     public String multipleSearchUserMap(@RequestParam Map<String, String> params){
         return "Searching for user: " + params.get("firstName") + " " + params.get("lastName");
+    }
+
+    /*
+    @GetMapping("/user/{userId}")
+    public String getUser(@PathVariable String userId){
+        return "User with ID: " + userId + " is Bertold";
+    }
+
+    @GetMapping("/user/{userId}/post/{postId}")
+    public String getUserPost(@PathVariable(name = "userId") String id, @PathVariable(name = "postId") String postId){
+        return "User with ID: " + id + " posts " + postId;
+    }
+    */
+
+    @GetMapping({"/user/{userId}/post/{postId}", "/user/{userId}" })
+    public String getUserPostVariableLength(@PathVariable(name = "userId") String id, @PathVariable(required = false) String postId){
+        return "getUserPostVariableLength is returned: User with ID: " + id + " posts " + postId;
     }
 }
